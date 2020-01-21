4162c1b02192e0402ae7b78bdd0c758cf9e26bae
Mathieu <mathieu.hebbrecht@gmail.com>
2020-01-21 15:41:37 +0100

    Update ArtnetReceive.ino

--- examples/Basic/ArtnetReceive/ArtnetReceive.ino
+++ examples/Basic/ArtnetReceive/ArtnetReceive.ino
@@ -9,18 +9,62 @@ This example may be copied under the terms of the MIT license, see the LICENSE f
 #include <EthernetUdp.h>
 #include <SPI.h>
 
+// Checked with schematic
+#define WIZ_CS        0
+#define WIZ_RESET     1
+#define WIZ_INT       16
+#define HAPPY_LED     14
+#define FRAME_SYNC    17
+#define WIZ_MOSI      11
+#define WIZ_MISO      12
+#define WIZ_SCK       13
+
 Artnet artnet;
 
 // Change ip and mac address for your setup
-byte ip[] = {10, 0, 1, 199};
-byte broadcast[] = {10, 0, 1, 255};
+byte ip[] = {192, 168, 0, 15};
+byte broadcast[] = {192, 255, 255, 255};
 byte mac[] = {0x04, 0xE9, 0xE5, 0x00, 0x69, 0xEC};
 
 void setup()
 {
+  // Port directions
+  pinMode(HAPPY_LED, OUTPUT);
+  pinMode(WIZ_RESET, OUTPUT);
+  pinMode(WIZ_CS, OUTPUT);
+  pinMode(WIZ_INT, INPUT);
+  pinMode(FRAME_SYNC, INPUT);
+
+  // Port Init
+  digitalWrite(WIZ_CS, HIGH);     // De-select Wiz850io
+  digitalWrite(WIZ_RESET, LOW);   //Start Wiz850io Reset
+  delay(50);
+  digitalWrite(WIZ_RESET, LOW);   //End Wiz850io Reset
+  digitalWrite(WIZ_CS, LOW);      // Select Wiz850io
+  
+  
   Serial.begin(115200);
+  while (!Serial) 
+  {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println( "Compiled: " __DATE__ ", " __TIME__ ", " __VERSION__);
+  
+  delay(50);
   artnet.begin(mac, ip);
   artnet.setBroadcast(broadcast);
+  delay(100);
+  
+  //Check if Wiz850io is present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("No Wiz850io (or 820io) found. Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
 }
 
 void loop()
@@ -30,7 +74,7 @@ void loop()
   {
     Serial.println("POLL");
   }
-  if (r == ART_DMX)
+  if(r == ART_DMX)
   {
     // print out our data
     Serial.print("universe number = ");
@@ -49,4 +93,3 @@ void loop()
     Serial.println();
   }
 }
-
